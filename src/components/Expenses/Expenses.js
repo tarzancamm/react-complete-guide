b21ebad7de52b39c545cea0fadcbda742ea9bdfa
Cameron Miner <cameronsminer@gmail.com>
2022-11-26 10:29:06 -0700

    Added conditionals and made site dynamic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,41 +1,30 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 //Attribute names (title, amount, date) can be whatever you want, but have them make sense. In this case I just chose what they are called in the objects above.
 const Expenses = (props) => {
-  const [changedYear, setChangedYear] = useState('2020')
+  const [changedYear, setChangedYear] = useState("2020");
 
   const changeYearHandler = (selectedYear) => {
-    setChangedYear(selectedYear)
-  }
+    setChangedYear(selectedYear);
+  };
+
+  const filteredExpenses = props.allExpenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === changedYear;
+  });
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={changedYear} onChangeYear={changeYearHandler} />
-        <ExpenseItem
-          title={props.allExpenses[0].title}
-          amount={props.allExpenses[0].amount}
-          date={props.allExpenses[0].date}
+        <ExpensesFilter
+          selected={changedYear}
+          onChangeYear={changeYearHandler}
         />
-        <ExpenseItem
-          title={props.allExpenses[1].title}
-          amount={props.allExpenses[1].amount}
-          date={props.allExpenses[1].date}
-        />
-        <ExpenseItem
-          title={props.allExpenses[2].title}
-          amount={props.allExpenses[2].amount}
-          date={props.allExpenses[2].date}
-        />
-        <ExpenseItem
-          title={props.allExpenses[3].title}
-          amount={props.allExpenses[3].amount}
-          date={props.allExpenses[3].date}
-        />
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
